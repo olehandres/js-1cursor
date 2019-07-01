d412001affb647e77893f23f03e95075712dd81f
Oleh <olehandres@icloud.com>
2019-07-01 16:32:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const uah = 8;
+function value (dol) {
+  const value = dol*uah;
+  return value
+}
+console.log(value(10))
+//--------------------------------------------------------------
+
+//TASK 3
+
+function printStairs() {
+    var n = 6;
+    var star, sky, space;
+
+    for (var i = 0; i < n; i++){
+        star = n - 1 - i;
+        sky = i + 1;
+        space = ' '.repeat(space) + '#'.repeat(hash);
+        console.log(stair);    
+    }  
+}
+
+printstairs(6);
+
+//----------------------------------------------------------------
+// TASK 5
+function min(a, b, c) {
+    if (a<b && a<c)
+    return a;
+    else if (b<a && b<c)
+    return b;
+    else if (c<a && c<b)
+    return c;
+}
+min(10,5,11)
+//---------------------------------------------------------------
+// TASK 2
+function reverse(str) {
+    const word =str.split('').reverse().join('');
+    return reverse;
+}
+console.log(reverse("cursor"));
